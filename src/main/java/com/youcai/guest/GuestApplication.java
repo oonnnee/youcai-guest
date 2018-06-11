508e22bc431ed82cd141383ca50727c3b80cf255
one <1729711680@qq.com>
2018-06-12 06:57:49 +0800

    '-'

---
+++ src/main/java/com/youcai/guest/GuestApplication.java
@@ -0,0 +1,12 @@
+package com.youcai.guest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuestApplication.class, args);
+    }
+}
